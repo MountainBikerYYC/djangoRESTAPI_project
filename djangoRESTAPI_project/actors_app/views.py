a08130af2c068182bfc0bb9ec540c684dd22d9ba
MountainBikerYYC <ywang9696@gmail.com>
2023-08-04 17:19:25 -0600

    Added models, serializer, get request

--- djangoRESTAPI_project/actors_app/views.py
+++ djangoRESTAPI_project/actors_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import actorSerializers
+from .models import Actor
+from rest_framework.response import Response
 
-# Create your views here.
+class ActorsList(APIView):
+
+    def get(self, request):
+        actors = Actor.objects.all()
+        serialize = actorSerializers(actors, many=True)
+        return Response(serialize.data)
